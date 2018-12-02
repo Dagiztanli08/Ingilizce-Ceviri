d2da446505d6400f970332fe621823826ddd59c8
Dağıztanlı <40044589+Dagiztanli08@users.noreply.github.com>
2018-12-02 11:10:59 +0200

    Update Çeviri.py

--- Çeviri.py
+++ Çeviri.py
@@ -1,1 +1,32 @@
+import time
+import pyperclip
+from googletrans import Translator
+from win10toast import ToastNotifier
+
+
+
+print("""
+CREATOR BY;
+    EMRE ATAKURU
+            CYBER-WARRİOR
+""")
+
+time.sleep(3)
+
+son_deger = ""
+while True:
+    translates = Translator()
+    kopyalanan_deger = pyperclip.paste()
+    if kopyalanan_deger != son_deger:
+        son_deger = kopyalanan_deger
+        print ("Değer Değişti: %s" % str(son_deger))
+        c = translates.translate(son_deger,dest="tr").text
+        print("""
+         [>] {0} == {1}
+        ----------------------------------------------------
+        """.format(son_deger.upper(),c.upper()))
+                
+        toast = ToastNotifier()
+        toast.show_toast("CEVİRİ {0}".format(pyperclip.paste()).lower(),msg=c)
+
 
